ff5c89cd13b8e42ac91184ef1280709683cc6175
Eric Holsinger <eric.holsinger@gmail.com>
2015-04-07 18:57:55 -0400

    left/right movement and sprite flipping

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,22 +4,52 @@ using System.Collections;
 public class PlayerBehavior : MonoBehaviour {
 
 	public int jumpHeight;
+	public int moveSpeed;
 	public int maxJumps; // maximum number of jumps
 	
 	private int numJumps; // number of jumps
 
+	private bool facingRight; // is our character facing right?
+
 	// Use this for initialization
 	void Start () {
 		numJumps = 0;
+		facingRight = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space) && CanJump ())
 		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+			float x = GetComponent<Rigidbody2D>().velocity.x;
+
+			GetComponent<Rigidbody2D>().velocity = new Vector2(x, jumpHeight);
 
 			++numJumps;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			float y = GetComponent<Rigidbody2D>().velocity.y;
+
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, y);
+
+			// if facing to the right, flip to face left
+			if (facingRight) {
+				Flip ();
+			}
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			float y = GetComponent<Rigidbody2D>().velocity.y;
+			
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, y);
+
+			// if not facing to the right, flip to face right
+			if (!facingRight) {
+				Flip ();
+			}
 		}
 	}
 
@@ -34,5 +64,20 @@ public class PlayerBehavior : MonoBehaviour {
 	bool CanJump()
 	{
 		return numJumps < maxJumps;
+	}
+
+	// flips the sprite to face the opposite direction on the x-axis
+	void Flip()
+	{
+		Vector3 flipScale; 
+
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+
+		flipScale = rigidBody.transform.localScale; // current scale vector
+		flipScale.x *= -1; // flip horizontally, along x-axis
+
+		rigidBody.transform.localScale = flipScale;
+
+		facingRight = !facingRight; // facing opposite direction, now
 	}
 }
