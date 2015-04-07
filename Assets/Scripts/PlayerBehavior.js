5f4e5d3311c92c1e96642c375d0c10aaa16f3814
Eric Holsinger <eric.holsinger@gmail.com>
2015-04-07 19:29:12 -0400

    left/right movement and sprite flipping

--- Assets/Scripts/PlayerBehavior.js
+++ Assets/Scripts/PlayerBehavior.js
@@ -1,19 +1,46 @@
 ﻿#pragma strict
 
 public var jumpHeight = 0;
+public var moveSpeed = 0;
 public var maxJumps = 0; // maximum number of jumps
 
 private var numJumps = 0; // number of current jumps
+private var facingRight = true; // initially sprite faces to the right
 
 function Start () {
 
 }
 
 function Update () {
+	var x;
+	var y;
+	
 	if (Input.GetKeyDown(KeyCode.Space) && CanJump()) {
-		GetComponent(Rigidbody2D).velocity = new Vector2(0, jumpHeight);
+		x = GetComponent(Rigidbody2D).velocity.x;
+		
+		GetComponent(Rigidbody2D).velocity = new Vector2(x, jumpHeight);
 		
 		++numJumps;
+	}
+	
+	if (Input.GetKey (KeyCode.LeftArrow)) {
+		y = GetComponent(Rigidbody2D).velocity.y;
+		
+		GetComponent(Rigidbody2D).velocity = new Vector2(-moveSpeed, y);
+		
+		if (facingRight) {
+			Flip();
+		}
+	}
+	
+	if (Input.GetKey (KeyCode.RightArrow)) {
+		y = GetComponent(Rigidbody2D).velocity.y;
+		
+		GetComponent(Rigidbody2D).velocity = new Vector2(moveSpeed, y);
+		
+		if (!facingRight) {
+			Flip();
+		}
 	}
 }
 
@@ -25,4 +52,19 @@ function OnCollisionEnter2D (coll : Collision2D) {
 
 function CanJump() {
 	return numJumps < maxJumps;
+}
+
+// flip sprite along x-axis
+function Flip() {
+	var flipScale : Vector3;
+	var rigidbody : Rigidbody2D;
+	
+	rigidbody = GetComponent(Rigidbody2D);
+	
+	flipScale = rigidbody.transform.localScale;
+	flipScale.x *= -1; // flip horizontally
+	
+	rigidbody.transform.localScale = flipScale;
+	
+	facingRight = !facingRight; // facing opposite direction
 }
